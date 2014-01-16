4ef345f0a869edb6a6959b4df960f6c09f2f6d7f
StevenZabolotny <steven.zabolotny@cslab1-12.stuy.edu>
2014-01-16 10:17:00 -0500

    Gui Stuff

---
+++ Final.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.awt.event.*;
+
+public class Final implements ActionListener {
+    JPanel titlePanel, gamePanel, buttonPanel;
+    JLabel titleLabel, turnLabel, oneTypeLabel, twoTypeLabel;
+    JButton newGameButton;
+    int turn;
+    String oneType,twoType;
+
+    public JPanel createContentPane() {
+	turn = 0;
+	oneType = "";
+	twoType = "";
+
+	JPanel totalGUI = new JPanel();
+	totalGUI.setLayout(null);
+	
+	titlePanel = new JPanel();
+	titlePanel.setLayout(null);
+	titlePanel.setLocation(10,0);
+	titlePanel.setSize(500,500);
+	totalGUI.add(titlePanel);
+	titleLabel = new JLabel("8-ball Pool");
+	titleLabel.setLocation(250,0);
+	titleLabel.setSize(100,20);
+	titleLabel.setHorizontalAlignment(0);
+	titleLabel.setForeground(Color.red);
+	titlePanel.add(titleLabel);
+
+	gamePanel = new JPanel();
+	gamePanel.setLayout(null);
+	gamePanel.setLocation(10,40);
+	gamePanel.setSize(800,500);
+	gamePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+	totalGUI.add(gamePanel);
+
+	buttonPanel = new JPanel();
+	buttonPanel.setLayout(new FlowLayout());
+	buttonPanel.setLocation(900,40);
+	buttonPanel.setSize(200,40);
+	totalGUI.add(buttonPanel);
+	turnLabel = new JLabel("" + turn);
+	turnLabel.setHorizontalAlignment(0);
+	titlePanel.add(turnLabel);	
+	oneTypeLabel = new JLabel(oneType);
+	oneTypeLabel.setHorizontalAlignment(0);
+	oneTypeLabel.setForeground(Color.red);
+	buttonPanel.add(oneTypeLabel);
+	twoTypeLabel = new JLabel(twoType);
+	twoTypeLabel.setHorizontalAlignment(0);
+	twoTypeLabel.setForeground(Color.blue);
+	buttonPanel.add(twoTypeLabel);
+	newGameButton = new JButton("New Game");
+	newGameButton.addActionListener(this);
+	buttonPanel.add(newGameButton);
+	
+	return totalGUI;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	if(e.getSource() == newGameButton) {
+	}
+    }
+
+    public static void createAndShowGUI() {
+	JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("8-ball Pool!");
+
+        //Create and set up the content pane.
+        Final gui = new Final();
+        frame.setContentPane(gui.createContentPane());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 1500);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+		    createAndShowGUI();
+		}
+	    });
+    }
+}
+	    
